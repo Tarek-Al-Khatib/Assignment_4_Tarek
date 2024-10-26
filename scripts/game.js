3f3d38f3b8a981826f624322daee5068845cab25
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-10-26 13:06:02 +0300

    adding logic behind finding the letters and displaying the word

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,4 @@
-const words = [
+var words = [
   "apple",
   "banana",
   "cherry",
@@ -30,7 +30,40 @@ const words = [
 //https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
 var randomIndex = Math.floor(Math.random() * words.length);
 var wordGame = words[randomIndex];
-console.log(wordGame);
 
 var wrongGuesses = 0;
 var letters = document.querySelectorAll(".letter");
+var hangContainer = document.getElementById("hang");
+var answerContainer = document.getElementById("answer-section");
+
+var hangmanImages = [
+  "./assets/head.svg",
+  "./assets/body.svg",
+  "./assets/left-hand.svg",
+  "./assets/right-hand.svg",
+  "./assets/left-leg.svg",
+  "./assets/right-leg.svg",
+];
+
+//https://stackoverflow.com/questions/49680484/how-to-add-one-event-listener-for-all-buttons
+letters.forEach((letter) => {
+  var letterText = letter.innerText;
+  letter.addEventListener("click", function () {
+    letterClick(letterText);
+  });
+});
+
+//chatgpt
+let correctGuesses = new Set();
+function displayWord() {
+  answerContainer.innerHTML = wordGame
+    .split("")
+    .map((letter) => (correctGuesses.has(letter) ? letter : "_"))
+    .join(" ");
+}
+displayWord();
+
+function letterClick(letter) {
+  var clickedLetter = [...letters].find((t) => t.innerText == letter);
+  console.log(clickedLetter.innerHTML);
+}
